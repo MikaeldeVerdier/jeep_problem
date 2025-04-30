54818785a74624f44a7043af7187ce77e3b05978
Mikael de Verdier <mikael.deverdier@gmail.com>
2025-04-30 21:02:33 +0200

    added option to only save last frame; minor improvements

--- jeep_solver.py
+++ jeep_solver.py
@@ -11,11 +11,15 @@ class JeepSolver:
         self.save_path = save_path
 
         create_path(save_path)
+        self.images_path = os.path.join(save_path, "images")
+        create_path(self.images_path)
+        self.animations_path = os.path.join(save_path, "animations")
+        create_path(self.animations_path)
 
     def get_depo(self, k, n):
         factor = 1 / (2 * n - 2 * k + 1)
         new_x = factor + (self.fuel_dumps[-1][0] if len(self.fuel_dumps) else 0)
-        new_volume = (2 * n - 2 * k - 1) * factor
+        new_volume = 1 - 2 * factor  # (2 * n - 2 * k - 1) * factor
         new_withdraw_size = factor
 
         new_dump = [new_x, new_volume, new_withdraw_size]
@@ -35,7 +39,7 @@ class JeepSolver:
         total_dist = 1 + np.sum(self.fuel_dumps[:, 2])
         self.fuel_dumps = np.vstack((self.fuel_dumps, [total_dist, 0, 0]))
 
-        print(f"Final distance crossed: {total_dist}")
+        print(f"Final distance crossed for n={n} is {total_dist}")
 
     def generate_trips(self, dists, slow_factor, reverse=False):
         trips = []
@@ -49,7 +53,7 @@ class JeepSolver:
 
         return trips
 
-    def visualize_trip(self, slow_factor=jeep_solver_config.slow_factor, fps=jeep_solver_config.fps, paus_time=jeep_solver_config.paus_time):
+    def visualize_trip(self, slow_factor=jeep_solver_config.slow_factor, fps=jeep_solver_config.fps, paus_time=jeep_solver_config.paus_time, do_animation=jeep_solver_config.do_animation):
         xs = []
         fuels = []
         trips = []
@@ -114,9 +118,9 @@ class JeepSolver:
 
             fuel_changes.extend(zip(start_point + net_fuel_change_indices[0] + 1, net_fuel_changes[net_fuel_change_indices]))
 
-        self.plot_visulization(np.array(xs), np.array(fuels), np.array(trips), fuel_changes, fps=fps, paus_time=paus_time)
+        self.plot_visulization(np.array(xs), np.array(fuels), np.array(trips), fuel_changes, fps, paus_time, do_animation)
 
-    def plot_visulization(self, xs, fuels, trips, fuel_changes, fps, paus_time):
+    def plot_visulization(self, xs, fuels, trips, fuel_changes, fps, paus_time, do_animation):
         fig, axs = plt.subplots(len(trips) + 1, figsize=(7, 2 + 1.5 * len(trips)), sharex=True)
         axs[0].set_xlim(0, np.max(xs))
         height = 1.5
@@ -138,32 +142,16 @@ class JeepSolver:
             axs[i + 1].set_ylabel("Fuel Level")
 
         plt.xlabel("Displacement")
-
-        """  # Only stops at midpoints of trips
-        paus_frames = int(fps * paus_time)
-        index_script = []
-        for i, trip in enumerate(trips[:, 1]):
-            prev_trip = trips[i - 1][1] if i > 0 else 0
-            trip_script = list(range(prev_trip, trip)) + [trip - 1] * paus_frames
-
-            index_script += trip_script
-        """
-
-        # """  # Stops at every fuel change
-        paus_frames = int(fps * paus_time)
-        index_script = []
-        for i, fuel_change in enumerate(fuel_changes):
-            prev_fuel_change = fuel_changes[i - 1][0] if i > 0 else 0
-            index_script += list(range(prev_fuel_change, fuel_change[0] + 1)) + [fuel_change[0]] * paus_frames
-
-        index_script += list(range(fuel_changes[-1][0] if len(fuel_changes) else 0, len(xs))) + [len(xs) - 1] * paus_frames
-        # """
+        plt.tight_layout()
 
         def animate(i):
             index = index_script[i]
 
             all_trips = np.array(trips)
             trip_index = int(np.nonzero((index >= all_trips[:, 0]) & (index < all_trips[:, 2]))[0][0])
+            for trip_i, trip in enumerate(all_trips[:trip_index]):
+                lines[trip_i].set_data(xs[trip[0]:trip[2] + 1], fuels[trip[0]:trip[2] + 1])
+
             start_index = int(all_trips[trip_index][0])
             lines[trip_index].set_data(xs[start_index:index + 1], fuels[start_index:index + 1])
 
@@ -191,15 +179,40 @@ class JeepSolver:
 
             return point, text
 
-        plt.tight_layout()
-        # plt.legend()
+        """  # Only stops at midpoints of trips
+        paus_frames = int(fps * paus_time)
+        index_script = []
+        for i, trip in enumerate(trips[:, 1]):
+            prev_trip = trips[i - 1][1] if i > 0 else 0
+            trip_script = list(range(prev_trip, trip)) + [trip - 1] * paus_frames
+
+            index_script += trip_script
+        """
+
+        # """  # Stops at every fuel change
+        paus_frames = int(fps * paus_time)
+        index_script = []
+        for i, fuel_change in enumerate(fuel_changes):
+            prev_fuel_change = fuel_changes[i - 1][0] if i > 0 else 0
+            index_script += list(range(prev_fuel_change, fuel_change[0] + 1)) + [fuel_change[0]] * paus_frames
+
+        index_script += list(range(fuel_changes[-1][0] if len(fuel_changes) else 0, len(xs))) + [len(xs) - 1] * paus_frames
+        # """
+
+        if not do_animation:
+            animate(len(index_script) - 1)
+
+            image_path = os.path.join(self.images_path, f"image_{len(trips)}trips.png")
+            plt.savefig(image_path)
+
+            return
 
         ani = animation.FuncAnimation(fig, animate, frames=len(index_script))
-        animation_path = os.path.join(self.save_path, f"animation_{len(trips)}trips.gif")
+        animation_path = os.path.join(self.animations_path, f"animation_{len(trips)}trips.gif")
         ani.save(animation_path, fps=fps)
 
 
 if __name__ == "__main__":
     solver = JeepSolver()
     solver.simulate_trip()
-    # solver.visualize_trip()
+    solver.visualize_trip()
--- jeep_solver_config.py
+++ jeep_solver_config.py
@@ -1,10 +1,11 @@
-# Visualizer arguments
+# Solver arguments
 save_path = "visualizations"  # Where to save results
 
-# Simulation parameters
-n = 1  # Number of trips
+# Simulation arguments
+n = 3  # Number of trips
 
-# Visualization parameters
+# Visualization arguments
 slow_factor = 75  # Slow down factor for visualization
 fps = 30  # Frames per second of animation
 paus_time = 1.5  # Pause time on fuel change
+do_animation = True  # Whether to animate the trip
